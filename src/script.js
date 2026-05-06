fe6cbd01a348844e69c46a0439139701c1b2fbc6
lauren-ana <lauren_anahera@hotmail.com>
2026-05-07 08:04:32 +1000

    Created day/time display function

--- src/script.js
+++ src/script.js
@@ -1,28 +1,44 @@
 function refreshTemperature(response) {
-  console.log(response.data);
   let currentTemp = response.data.temperature.current;
   let degreeElement = document.querySelector("#forecast-degree");
-
   let cityInputElement = document.querySelector("#forecast-city");
+  let conditionElement = document.querySelector("#condition-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+
   cityInputElement.innerHTML = response.data.city;
-
-  let condition = document.querySelector("#condition-description");
-  condition.innerHTML = response.data.condition.description;
-  console.log(response.data.condition.description);
-
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.temperature.humidity;
-
-  let windSpeed = document.querySelector("#wind-speed");
-  windSpeed.innerHTML = Math.round(response.data.wind.speed);
-
+  timeElement.innerHTML = displayDate(date);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   degreeElement.innerHTML = Math.round(currentTemp);
+}
+
+function displayDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "9ca9f78o333f17d1c9f4d8ae5b0f036t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
-  console.log(apiUrl);
 
   axios.get(apiUrl).then(refreshTemperature);
 }
