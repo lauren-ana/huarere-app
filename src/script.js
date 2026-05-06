9328159ef23cda8e1932e928ae756e63f9b9b996
lauren-ana <lauren_anahera@hotmail.com>
2026-05-06 14:03:08 +1000

    Added search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,28 @@
+function refreshTemperature(response) {
+  let currentTemp = response.data.temperature.current;
+  let degreeElement = document.querySelector(".forecast-degree");
+
+  let cityInputElement = document.querySelector("#forecast-city");
+  cityInputElement.innerHTML = response.data.city;
+
+  degreeElement.innerHTML = Math.round(currentTemp);
+}
+
+function searchCity(city) {
+  let apiKey = "9ca9f78o333f17d1c9f4d8ae5b0f036t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+
+  axios.get(apiUrl).then(refreshTemperature);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityInputElement = document.querySelector("#forecast-city");
-  cityInputElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchCityForm = document.querySelector("#search-form");
 searchCityForm.addEventListener("submit", handleSearch);
+
+searchCity("Auckland");
